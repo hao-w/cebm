0aa6fab8e34993b9443733ad4aaf1ac98b9d21ae
hao-w <wuhaomxhy@gmail.com>
2020-08-06 19:47:39 -0400

    .

--- vanilla/nets/energy_function_babak_cnn.py
+++ vanilla/nets/energy_function_babak_cnn.py
@@ -5,9 +5,10 @@ class Energy_function(nn.Module):
     """
     A CNN architecture used by Babak
     """
-    def __init__(self, negative_slope=0.01):
+    def __init__(self, negative_slope=0.05):
         super(self.__class__, self).__init__()
         
+
         self.cnn = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(negative_slope=negative_slope, inplace=True),
