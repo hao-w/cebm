204d7b60bce0cf93f46f627532973b5bb7963d1a
hao-w <wuhaomxhy@gmail.com>
2021-03-19 15:34:44 -0400

    removed checkpoints

--- cebm/.ipynb_checkpoints/cebm_svhn-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python3 cebm_sgld.py  --seed=1 --device=0 --dataset=svhn --data_dir=../../sebm_data/ --batch_size=100  \
---num_epochs=10 --buffer_init --buffer_dup_allowed  \
---hidden_dim=[128]
