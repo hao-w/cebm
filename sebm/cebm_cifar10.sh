ceca07fb918eec397aa7e7a5c4d51900a161c507
hao-w <wuhaomxhy@gmail.com>
2021-03-19 15:23:32 -0400

    renamed the subfolder

--- sebm/cebm_cifar10.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 cebm_sgld.py --ss=2 --seed=123 --device=1 --dataset=cifar10 --data_dir=../../sebm_data/ --batch_size=100 --data_noise_std=1e-2 --optimizer=Adam --lr=1e-4 --num_epochs=100 --buffer_init --buffer_dup_allowed --sgld_noise_std=1e-2 --sgld_lr=2.0 --sgld_num_steps=50 --regularize_factor=0 --activation=LeakyReLU --leak=0.2 --arch=simplenet5
--- sebm/gan_fmnist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-python3 dcgan.py --seed=1 --device=1 --dataset=fashionmnist --data_dir=../../sebm_data/ --batch_size=100 --optimizer=Adam --lr=2e-4  --num_epochs=150 
-
-python3 dcgan_gmm.py --seed=1 --device=1 --dataset=fashionmnist --data_dir=../../sebm_data/ --batch_size=100 --optimizer=Adam --lr=2e-4  --num_epochs=150 
-
-# python3 bigan_gmm.py --seed=1 --device=1 --dataset=fashionmnist --data_dir=../../sebm_data/ --batch_size=100 --optimizer=Adam --lr=2e-4  --num_epochs=150 
