e1178b58ae09bde69a45268ddf167e05201fc447
hao-w <wuhaomxhy@gmail.com>
2021-06-07 22:58:54 -0400

    .

---
+++ cebm/train/train_cebm2.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+model=CEBM
+device=cuda:1
+likelihood=gaussian
+
+python3 train_ebm.py --data=cifar10 \
+                     --model_name=$model \
+                     --channels=[64,128,256,512] \
+                     --kernels=[3,4,4,4] \
+                     --strides=[1,2,2,2] \
+                     --paddings=[1,1,1,1] \
+                     --hidden_dims=[1024] \
+                     --device=$device 
+
+python3 train_ebm.py --data=svhn \
+                     --model_name=$model \
+                     --channels=[64,128,256,512] \
+                     --kernels=[3,4,4,4] \
+                     --strides=[1,2,2,2] \
+                     --paddings=[1,1,1,1] \
+                     --hidden_dims=[1024]
+                     --device=$device
