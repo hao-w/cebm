6a8a88879a15e71fe58003920b61a9c379867f8a
Hao Wu <hao@Haos-MacBook-Pro.local>
2021-07-21 16:12:37 -0400

    .

--- cebm/data.py
+++ cebm/data.py
@@ -17,7 +17,7 @@ from torch.utils.data import Dataset, TensorDataset, DataLoader, ConcatDataset
     
 def setup_data_loader(data, data_dir, num_shots, batch_size, train, normalize, shuffle=True, shot_random_seed=None):
     """
-    return a dataloader 
+    This method constructs a dataloader for several commonly-used image datasets
     argument:
         data: name of the dataset
         data_dir: path of the dataset
@@ -28,11 +28,8 @@ def setup_data_loader(data, data_dir, num_shots, batch_size, train, normalize, s
         normalize: if True, rescale the pixel values to [-1, 1]
         shot_random_seed: the random seed used when num_shot != -1
     """
-    dataset_path = os.path.join(data_dir, data)
-    if not os.path.exists(dataset_path):
-        os.makedirs(dataset_path)
         
-    if data in ['mnist', 'fmnist', 'emnist', 'constant_grayscale', 'omniglot']:
+    if data in ['mnist', 'fashionmnist', 'emnist', 'constant_grayscale']:
         img_h, img_w, n_channels = 28, 28, 1
         transform = transforms.Compose([transforms.ToTensor(),
                                         transforms.Normalize((0.5,), (0.5,))])
@@ -46,43 +43,44 @@ def setup_data_loader(data, data_dir, num_shots, batch_size, train, normalize, s
         del transform.transforms[-1] # should not normalize for VAEs
 
     if data == 'mnist':
-        dataset = datasets.MNIST(dataset_path, train=train, transform=transform, download=True)
-    elif data == 'fmnist':
-        dataset = datasets.FashionMNIST(dataset_path, train=train, transform=transform, download=True)
+        dataset = datasets.MNIST(data_dir, train=train, transform=transform, download=True)
+    elif data == 'fashionmnist':
+        dataset = datasets.FashionMNIST(data_dir, train=train, transform=transform, download=True)
     elif data == 'emnist':
-        dataset = datasets.EMNIST(dataset_path, split='digits', train=train, transform=transform, download=True)
+        dataset = datasets.EMNIST(data_dir, split='digits', train=train, transform=transform, download=True)
     elif data == 'cifar10':
-        dataset = datasets.CIFAR10(dataset_path, train=train, transform=transform, download=True)
+        data_dir = join(data_dir, 'CIFAR10')
+        dataset = datasets.CIFAR10(data_dir, train=train, transform=transform, download=True)
     elif data == 'svhn':
+        data_dir = join(data_dir, 'SVHN')
         if train:
-            train_part = datasets.SVHN(dataset_path, split='train', transform=transform, download=True)
-            extra_part = datasets.SVHN(dataset_path, split='extra', transform=transform, download=True)
+            train_part = datasets.SVHN(data_dir, split='train', transform=transform, download=True)
+            extra_part = datasets.SVHN(data_dir, split='extra', transform=transform, download=True)
             dataset = ConcatDataset([train_part, extra_part])
         else:
-            dataset = datasets.SVHN(dataset_path, split='test', transform=transform, download=True)
-    elif data == 'celeba':
-        dataset = datasets.CelebA(dataset_path, split='train', transform=transform, download=True, target_type='attr')
+            dataset = datasets.SVHN(data_dir, split='test', transform=transform, download=True)
     elif data == 'texture':
+        data_dir = join(data_dir, 'texture')
         try:
-            dataset = datasets.ImageFolder(root=dataset_path+'/dtd/images/', transform=transform)
+            dataset = datasets.ImageFolder(root=dataset_dir+'/dtd/images/', transform=transform)
         except:
             import requests
             import tarfile
             url = 'https://www.robots.ox.ac.uk/~vgg/data/dtd/download/dtd-r1.0.1.tar.gz'
             r = requests.get(url)
-            with open(dataset_path + '/dtd-r1.0.1.tar.gz', 'wb') as f:
+            with open(data_dir + '/dtd-r1.0.1.tar.gz', 'wb') as f:
                 f.write(r.content)
             f.close()
-            my_tar = tarfile.open(dataset_path + '/dtd-r1.0.1.tar.gz')
-            my_tar.extractall(dataset_path)
+            my_tar = tarfile.open(data_dir + '/dtd-r1.0.1.tar.gz')
+            my_tar.extractall(data_dir)
             my_tar.close()
-            dataset = datasets.ImageFolder(root=dataset_path+'/dtd/images/', transform=transform)
+            dataset = datasets.ImageFolder(root=data_dir+'/dtd/images/', transform=transform)
     elif data == 'constant_rgb':
-        dataset = Constant(color_mode='rgb', root=dataset_path, transform=transform)
+        data_dir = join(data_dir, 'constant_rgb')
+        dataset = Constant(color_mode='rgb', root=data_dir, transform=transform)
     elif data == 'constant_grayscale':
-        dataset = Constant(color_mode='grayscale', root=dataset_path, transform=transform)
-    elif data == 'omniglot':
-        dataset = Omniglot_Concat(root=dataset_path, train=train, download=True, transform=transform)
+        data_dir = join(data_dir, 'constant_grayscale')
+        dataset = Constant(color_mode='grayscale', root=data_dir, transform=transform)
     else:
         raise NotImplementError
     
@@ -137,160 +135,3 @@ class Constant(VisionDataset):
 
     def __len__(self):
         return len(self.data)
-    
-def setup_omniglot_loader(data_dir, way, shot, train=True, normalize=True, split_idx=1200):
-    dataset_path = os.path.join(data_dir, 'omniglot')
-    if not os.path.exists(dataset_path):
-        os.makedirs(dataset_path)
-
-    img_h, img_w, n_channels = 28, 28, 1
-    transform = transforms.Compose([transforms.Resize((28,28)),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((0.5,), (0.5,))])
-    if not normalize:
-        del transform.transforms[-1] # should not normalize for VAEs
-#     _ = Omniglot_Concat(root=dataset_path, download=True, transform=transform)
-    task_loader = Task_Loader(data_dir, way, shot, train, split_idx)   
-    return task_loader, img_h, img_w, n_channels
-
-        
-class Omniglot_Concat(VisionDataset):
-    download_url_prefix = 'https://github.com/brendenlake/omniglot/raw/master/python'
-    zips_md5 = {
-        'images_background': '68d2efa1b9178cc56df9314c21c6e718',
-        'images_evaluation': '6b91aef0f799c5bb55b94e3f2daec811'
-    }
-
-    def __init__(
-            self,
-            root: str,
-            transform: Optional[Callable] = None,
-            target_transform: Optional[Callable] = None,
-            download: bool = False,
-    ) -> None:
-        super(Omniglot_Concat, self).__init__(root, transform=transform,
-                                       target_transform=target_transform)
-        
-        self.subsets = ['images_background', 'images_evaluation']
-        
-        if download:
-            self.download()
-
-        if not self._check_integrity():
-            raise RuntimeError('Dataset not found or corrupted.' +
-                               ' You can use download=True to download it')
-            
-        self._alphabets = sum([[os.path.join(subset, a) for a in list_dir(os.path.join(self.root, subset))] for subset in self.subsets], [])
-        
-        self._characters: List[str] = sum([[os.path.join(a, c) for c in list_dir(os.path.join(self.root, a))]
-                                           for a in self._alphabets], [])
-
-        self._character_images = [[(image, idx) for image in list_files(os.path.join(self.root, character), '.png')]
-                                  for idx, character in enumerate(self._characters)]
-
-#         self._character_images = [[(image, idx) for image in list_files(os.path.join(self.root, self._characters[idx]), '.png')] for idx in split_range]
-
-        self._flat_character_images: List[Tuple[str, int]] = sum(self._character_images, [])
-        self.processing()
-        
-        print('Omniglot full set, characters=%d, images=%d' % \
-              (self.__num_classes__(), len(self._flat_character_images)))
-            
-    def __num_classes__(self) -> int:
-        return int(len(self._flat_character_images) / 20)
-
-
-    def processing(self):
-        print('Processing raw data')
-        self.processed =  defaultdict(list)
-        for idx, character in enumerate(tqdm(self._characters)):
-            character_path = os.path.join(self.root, self._characters[idx])
-            for image_name in list_files(character_path, '.png'):
-                image = Image.open(os.path.join(character_path, image_name), mode='r').convert('L')
-                if self.transform:
-                    image = self.transform(image)
-                self.processed[idx].append(image)
-        torch.save(self.processed, os.path.join(self.root, 'processed.pt'))
-    
-    def __len__(self) -> int:
-        return len(self._flat_character_images)
-
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        """
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: (image, target) where target is index of the target character class.
-        """
-        image_name, character_class = self._flat_character_images[index]
-        image_path = os.path.join(self.target_folder, self._characters[character_class], image_name)
-        image = Image.open(image_path, mode='r').convert('L')
-
-        if self.transform:
-            image = self.transform(image)
-
-        if self.target_transform:
-            character_class = self.target_transform(character_class)
-
-        return image, character_class
-
-    def _check_integrity(self) -> bool:
-        for subset in self.subsets:
-            if not check_integrity(os.path.join(self.root, subset + '.zip'), self.zips_md5[subset]):
-                return False
-        return True
-
-    def download(self) -> None:
-        if self._check_integrity():
-            return    
-        for subset in self.subsets:
-            zip_filename = subset + '.zip'
-            url = self.download_url_prefix + '/' + zip_filename
-            download_and_extract_archive(url, self.root, filename=zip_filename, md5=self.zips_md5[subset])
-
-class Task_Loader():
-    def __init__(self, data_dir, way, shot, train, split_idx):
-        super().__init__()
-        self.data_dir = data_dir
-        self.way = way
-        self.shot = shot
-        self.train = train
-        self.split_idx = split_idx
-        
-    def generate_tasks(self):
-        """
-        return a list of tasks given way-shot
-        """
-        fullset = torch.load(os.path.join(self.data_dir, 'omniglot', 'processed.pt'))
-        if self.train:
-            train_episodes = []
-            class_groups = torch.randperm(self.split_idx).view(int(self.split_idx / self.way), self.way)
-            for g in class_groups:
-                task = []
-                for c in g:
-                    list_instances = fullset[c.item()]
-                    random.shuffle(list_instances)
-                    task += list_instances[:self.shot]
-                random.shuffle(task)
-                task = torch.stack(task, 0)
-                train_episodes.append(task)
-            return train_episodes
-        else:
-#             raise NotImplementedError('Need to implement task generator for test time')
-            test_episodes = []
-            test_split_idx = torch.randperm(1623 - self.split_idx) + self.split_idx
-            truncate_length = (len(test_split_idx) // self.way) * self.way
-            class_groups = test_split_idx[:truncate_length].view(int(truncate_length / self.way), self.way)
-            for g in class_groups:
-                task_images = []
-                task_labels = []
-                for c in g:
-                    list_instances = fullset[c.item()]
-                    random.shuffle(list_instances)
-                    task_images += list_instances[:self.shot]
-                    task_labels += [torch.ones(self.shot) * c] 
-                task_images = torch.stack(task_images, 0)
-                task_labels = torch.stack(task_labels, 0)
-                test_episodes.append((task_images, task_labels))
-            return test_episodes
--- cebm/utils.py
+++ cebm/utils.py
@@ -27,29 +27,15 @@ def create_exp_name(args):
             exp_name+= '_learn-prior'
     # EBMs 
     elif args.model_name in ['IGEBM', 'CEBM', 'CEBM_GMM']:
-        exp_name = '%s_d=%s_z=%s_lr=%s_act=%s_sgld_s=%s_n=%s_a=%s_dn=%s_reg=%s_seed=%d_arch=%s' % \
+        exp_name = '%s_d=%s_z=%s_lr=%s_act=%s_sgld_s=%s_n=%s_a=%s_dn=%s_reg=%s_seed=%d' % \
                     (args.model_name, args.data, args.latent_dim, args.lr, args.activation,
                      args.sgld_steps, args.sgld_noise_std, args.sgld_alpha, args.image_noise_std, 
-                     args.regularize_coeff, args.seed, args.arch)
+                     args.regularize_coeff, args.seed)
         if args.model_name == 'CEBM_GMM':
             exp_name += '_K=%d' % args.num_clusters
         if args.optimize_ib:
             exp_name += '_learn-prior'
-            
-    elif args.model_name == 'SEMI_CLF':
-        exp_name = '%s_d=%s_z=%s_lr=%s_act=%s_reg=%s_seed=%d' % \
-                    (args.model_name, args.data, args.latent_dim, args.lr, 
-                     args.reg_lambda, args.seed)  
-        
-    elif args.model_name == 'META_CEBM':
-        exp_name = 'META_CEBM_d=%s_z=%s' % (args.data, args.latent_dim)
     
-    elif args.model_name in ['CEBM_VERA', 'CEBM_GMM_VERA', 'IGEBM_VERA']:
-        exp_name = '%s_d=%s_z=%s_lamb=%s' % (args.model_name, args.data, args.latent_dim, args.lambda_ent)
-        if args.likelihood == 'cb':
-            exp_name += '_cb'
-        if args.optimize_ib:
-            exp_name += '_learn-ib'
     else:
         raise ValueError
         
@@ -85,27 +71,6 @@ def init_models(model_name, device, model_args, network_args):
         elif model_name == 'BIGAN_GMM': 
             gen = Generator_BIGAN_GMM(model_args['optimize_prior'], model_args['num_clusters'], **network_args)
         return {'disc': disc.to(device), 'enc': enc.to(device), 'gen': gen.to(device)}
-    
-    elif model_name == 'META_CEBM':
-        enc = META_CEBM_Omniglot(**network_args)
-        return {'ebm': enc.to(device)}
-    
-    elif model_name in ['CEBM_VERA', 'CEBM_GMM_VERA', 'IGEBM_VERA']:
-        if model_name == 'CEBM_VERA':
-            model = CEBM_Gaussian(**network_args) 
-#             network_args['activation'] = 'ReLU'
-            gen = Generator_VERA_GAN(**network_args)
-#             gen = Generator_VERA_Gaussian(**network_args)
-        elif model_name == 'CEBM_GMM_VERA':
-            model = CEBM_GMM(model_args['optimize_ib'], model_args['num_clusters'], **network_args)   
-            gen = Generator_VERA_GAN(**network_args)
-        elif model_name == 'IGEBM_VERA':
-            model = IGEBM(**network_args)
-            network_args['activation'] = 'ReLU'
-            gen = Generator_VERA_GAN(**network_args)
-#         xee_logsigma = torch.nn.Parameter((torch.ones(network_args['latent_dim'], device=device) * 0.1).log())
-        xee = Xee(device, network_args['latent_dim'], network_args['xee_init_sigma'])
-        return {'ebm': model.to(device), 'gen': gen.to(device), 'xee': xee}
 
 def save_models(models, filename, weights_dir="./weights"):
     checkpoint = {k: v.state_dict() for k, v in models.items()}
